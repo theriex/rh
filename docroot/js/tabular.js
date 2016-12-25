715ae7d62e34c0e41cc86da42962a28c33577821
Eric Parker <theriex@gmail.com>
2016-12-24 19:09:06 -0500

    added date parsing, minor date formatting updates

--- docroot/js/tabular.js
+++ docroot/js/tabular.js
@@ -3,16 +3,85 @@
 app.tabular = (function () {
     "use strict";
 
-    function display() {
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+                  "Sep", "Oct", "Nov", "Dec"];
+
+
+    function dateSpan (dobj, prefix) {
+        var sc, pd;
+        if(!dobj || !dobj.year) {
+            return ""; }
+        prefix = prefix || "";
+        sc = prefix? "tdspan" : "tdspanyo";
+        if(dobj.year > 0) {
+            pd = prefix + dobj.year; }
+        else {
+            pd = prefix + (dobj.year * -1) + " " + jt.tac2html(
+                ["span", {cla: "bcespan"}, "BCE"]); }
+        if(dobj.month) {
+            sc = "tdspan";
+            pd += months[dobj.month - 1]; }
+        if(dobj.day) {
+            pd += dobj.day; }
+        return jt.tac2html(["span", {cla: sc}, pd]);
+    }
+
+
+    function parseDate (pt) {
+        var date, mres;
+        date = pt.date;
+        pt.start = {};
+        //start year
+        pt.start.year = date.match(/^\d\d?\d?\d?/)[0];
+        date = date.slice(pt.start.year.length);
+        pt.start.year = +(pt.start.year);
+        if(date.indexOf(" BCE") >= 0) {
+            date = date.slice(date.indexOf(" BCE") + 4);
+            pt.start.year *= -1; }
+        //start month
+        mres = date.match(/^\-\d\d(\-|$)/);
+        if(mres) {
+            date = date.slice(3);
+            pt.start.month = +(mres[0].slice(1,3)); }
+        //start day
+        mres = date.match(/^\-\d\d(\-|$)/);
+        if(mres) {
+            date = date.slice(3);
+            pt.start.day = +(mres[0].slice(1,3)); }
+        //end year
+        if(date.indexOf("-") >= 0) {
+            date = date.slice(date.indexOf("-") + 1); }
+        mres = date.match(/^\d\d?\d?\d?/);
+        if(mres) {
+            date = date.slice(mres[0].length);
+            pt.end = {};
+            pt.end.year = +(mres[0]); }
+        //end month
+        mres = date.match(/^\-\d\d(\-|$)/);
+        if(mres) {
+            date = date.slice(3);
+            pt.end.month = +(mres[0].slice(1,3)); }
+        //end day
+        mres = date.match(/^\-\d\d/);
+        if(mres) {
+            date = date.slice(3);
+            pt.end.day = +(mres[0].slice(1,3)); }
+    }
+
+
+    function display () {
         var outdiv = jt.byId(app.dispdivid);
         outdiv.innerHTML = jt.tac2html(["div", {id: "tcontdiv"}]);
         app.data.pts.forEach(function (pt) {
             var linediv = document.createElement("div");
+            parseDate(pt);
             linediv.innerHTML = jt.tac2html(
                 ["div", {cla: "trowdiv"},
                  [["div", {cla: "trowdatediv"},
-                   [["span", {cla: "tcspan"}, "(" + pt.code + ") "],
-                    ["span", {cla: "tdspan"}, pt.date]]],
+                   [dateSpan(pt.start),
+                    ["br"],
+                    dateSpan(pt.end, "-"),
+                    ["span", {cla: "tcspan"}, " (" + pt.code + ") "]]],
                   ["div", {cla: "trowdescdiv"}, pt.text]]]);
             outdiv.appendChild(linediv); });
     }
