86b9e6a429e1f60e06075280bf95bf287c3edfd9
Eric Parker <theriex@gmail.com>
2017-01-10 22:49:33 -1000

    Interim cid conversion progress

--- docroot/js/cite.js
+++ docroot/js/cite.js
@@ -11,5 +11,9 @@ app.cite = {
          "https://en.wikipedia.org/wiki/Diego_Vel%C3%A1zquez_de_Cu%C3%A9llar",
          "https://en.wikipedia.org/wiki/Santa_Mar%C3%ADa_la_Antigua_del_Dari%C3%A9n"],
     L3: ["https://en.wikipedia.org/wiki/Antonio_de_Montesinos"],
-    
+    B3: ["https://en.wikipedia.org/wiki/San_Miguel_de_Gualdape"],
+    N16: ["http://www.smithsonianmag.com/people-places/what-became-of-the-taino-73824867"],
+    L6: ["http://www.smithsonianmag.com/people-places/what-became-of-the-taino-73824867"],
+    B6: ["http://library.brown.edu/haitihistory/1sr.html"],
+
 };
