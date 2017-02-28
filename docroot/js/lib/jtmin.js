036a2150f7fd4abfeddac46859764e885f9c4364
Eric Parker <theriex@gmail.com>
2017-02-28 14:16:01 -0500

    base framework static display for slavery vis

--- docroot/js/lib/jtmin.js
+++ docroot/js/lib/jtmin.js
@@ -703,7 +703,7 @@ var jtminjsDecorateWithUtilities = function (utilityObject) {
     //Override this function if you need your own custom tags.  If I've
     //missed anything generally useful, let me know.
     uo.isHTMLElement = function (elemtype) {
-        var elems = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr", "svg"];
+        var elems = ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "command", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr", "svg", "g", "path", "rect", "circle"];
         if (typeof elemtype !== "string") {
             return false;
         }
@@ -718,7 +718,8 @@ var jtminjsDecorateWithUtilities = function (utilityObject) {
     uo.isHTMLVoidElement = function (elemtype) {
         var voids = ["area", "base", "br", "col", "command", "embed",
                      "hr", "img", "input", "keygen", "link", "menuitem",
-                     "meta", "param", "source", "track", "wbr"];
+                     "meta", "param", "source", "track", "wbr", "path",
+                     "rect", "circle"];
         elemtype = (uo.safestr(elemtype)).trim().toLowerCase();
         if (voids.indexOf(elemtype) >= 0) {
             return true;
