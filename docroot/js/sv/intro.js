d985dba6c8dd8b9ad9188e93098635cc00a5d638
Eric Parker <theriex@gmail.com>
2017-07-20 23:03:05 -0400

    General sketch of intro suppvis minus bookmarking

--- docroot/js/sv/intro.js
+++ docroot/js/sv/intro.js
@@ -4,11 +4,11 @@
 app.intro = (function () {
     "use strict";
 
-    var sv = null,    //decorated suppvis from data.js 
-        tl = null,    //linear timeline settings and data
-        endf = null,  //callback for after visualization has finished
-        chart = {};   //container for svg references
-
+    var sv = null,       //decorated suppvis from data.js 
+        tl = null,       //linear timeline settings and data
+        endf = null,     //callback for after visualization has finished
+        chart = {},      //container for svg references
+        mobile = false;  //browser device flag
 
     function initHTMLContent () {
         jt.out("suppvisdiv", jt.tac2html(
@@ -18,7 +18,10 @@ app.intro = (function () {
               ["div", {id:"sv0timediv"}],
               ["div", {id:"sv0textdiv"}],
               ["div", {id:"sv0linkdiv"}],
-              ["div", {id:"sv0closediv"}]]]));
+              ["div", {id:"sv0closediv"}],
+              ["div", {id:"sv0txtdiv", 
+                       style:"position:absolute;left:30px;top:120px;" +
+                             "margin-right:30px;opacity:0.0;"}]]]));
     }
 
 
@@ -68,7 +71,7 @@ app.intro = (function () {
                       " L " + to.x3 + " " + to.y3 +
                       " Z")
             .style("fill", to.fill)
-            .style("stroke", to.fill)
+            .style("stroke", to.fill);
         return trg;
     }
 
@@ -111,7 +114,6 @@ app.intro = (function () {
 
 
     function animateBrackets (vu) {
-        var sw = 0.5, mat = {};
         vu.tim = {unfurl:chart.visunfurltime,
                   rot:{del:Math.round(0.9 * chart.visunfurltime),
                        dur:400}};
@@ -125,22 +127,185 @@ app.intro = (function () {
         vu.tim.mv = {del:vu.tim.rot.del + vu.tim.rot.dur,
                      dur:800};
         //shrink width
-        mat.scalex = 0.3;
+        vu.scalex = 0.3;
         vu.leftg.transition().delay(vu.tim.mv.del).duration(vu.tim.mv.dur)
-            .attr("transform", "scale(" + mat.scalex + ",1)");
+            .attr("transform", "scale(" + vu.scalex + ",1)");
         vu.rightg.transition().delay(vu.tim.mv.del).duration(vu.tim.mv.dur)
-            .attr("transform", "scale(" + mat.scalex + ",1)");
+            .attr("transform", "scale(" + vu.scalex + ",1)");
         //move to edges
-        mat.transx = (-1 * (vu.cx - vu.rect.w)) * mat.scalex;
+        vu.transx = (-1 * (vu.cx - vu.rect.w)) * vu.scalex;
         vu.lmg.transition().delay(vu.tim.mv.del).duration(vu.tim.mv.dur)
-            .attr("transform", "translate(" + mat.transx + ")");
+            .attr("transform", "translate(" + vu.transx + ")");
         //offset to move the contents to the right side within shrunken space
-        mat.rs = (vu.cx - vu.rect.w) * mat.scalex;
+        vu.rs = (vu.cx - vu.rect.w) * vu.scalex;
         //offset to move entire shrunken space to right side of total space
-        mat.ss = tl.width2 * (1 - mat.scalex);
-        mat.transx = mat.rs + mat.ss;
+        vu.ss = tl.width2 * (1 - vu.scalex);
+        vu.transx = vu.rs + vu.ss;
         vu.rmg.transition().delay(vu.tim.mv.del).duration(vu.tim.mv.dur)
-            .attr("transform", "translate(" + mat.transx + ")");
+            .attr("transform", "translate(" + vu.transx + ")");
+    }
+
+
+    function drawHorizontalLine (vu) {
+        var pad = 2;
+        vu.g.append("rect")
+            .attr("id", "horizontalline")
+            .attr("x", vu.cx - 1)
+            .attr("y", vu.svgh - (vu.rect.h / 2))
+            .attr("width", 1)
+            .attr("height", 3)
+            .style("fill", "#000")
+        .transition().delay(vu.tim.mv.del).duration(vu.tim.mv.dur)
+            .attr("x", (vu.rect.w * vu.scalex) + pad)
+            .attr("width", tl.width2 - 
+                  (2 * (vu.rect.w * vu.scalex)) - (2 * pad));
+    }
+
+
+    function drawUnlockedText (vu) {
+        var pad = 5;
+        vu.tim.txt = {del:vu.tim.mv.del + vu.tim.mv.dur - 200,
+                      dur:500};
+        vu.textcolor = "#000";
+        vu.textsize = "36px";
+        vu.textg = vu.g.append("g").attr("id", "textg");
+        vu.textg.append("text")
+            .attr("x", vu.cx)
+            .attr("y", vu.cy - (vu.rect.h * 3 / 4) - pad)
+            .attr("fill", vu.textcolor)
+            .attr("opacity", 0.0)
+            .style("font-size", vu.textsize)
+            .style("font-weight", "bold")
+            .style("text-anchor", "middle")
+            .text("Chronology")
+            .transition().delay(vu.tim.txt.del).duration(vu.tim.txt.dur)
+            .attr("opacity", 1.0);
+        vu.textg.append("text")
+            .attr("x", vu.cx)
+            .attr("y", vu.cy - (vu.rect.h / 4) + pad)
+            .attr("fill", vu.textcolor)
+            .attr("opacity", 0.0)
+            .style("font-size", vu.textsize)
+            .style("font-weight", "bold")
+            .style("text-anchor", "middle")
+            .text("Unlocked")
+            .transition().delay(vu.tim.txt.del).duration(vu.tim.txt.dur)
+            .attr("opacity", 1.0);
+    }
+
+
+    function computeEstimatedCompletionTime () {
+        var t = 0;
+        app.data.pts.forEach(function (pt) {
+            if(!pt.visited && !pt.sv) {
+                //takes me avg 11 seconds/pt. Figure double if unfamiliar.
+                t += 22; } }); 
+        app.data.suppvis.forEach(function (sv) {
+            if(!sv.visited) {
+                //figure 5 minutes for each suppvis on average
+                t += 5 * 60; } });
+        chart.vu.avgct = t;   //count in seconds
+    }
+
+
+    function updateCompletionTimeDisplay () {
+        var t, hr, min, sec;
+        chart.vu.avgct -= 1;
+        t = Math.max(chart.vu.avgct, 0);
+        sec = t % 60;
+        if(sec < 10) {
+            sec = "0" + sec; }
+        t = Math.floor(t / 60);
+        min = t % 60;
+        if(min < 10) {
+            min = "0" + min; }
+        hr = Math.floor(t / 60);
+        jt.out("sv0timediv", "Estimated completion time " + 
+               hr + ":" + min + ":" + sec);
+        setTimeout(updateCompletionTimeDisplay, 1000);
+    }
+
+
+    function showCloseButton () {
+        jt.out("sv0closediv", jt.tac2html(
+            ["button", {type:"button", cla:"ghostbutton",
+                        onclick:jt.fs("app.intro.close()")},
+             "Continue"]));
+    }
+
+
+    function close () {
+        var date, nowiso;
+        date = new Date();
+        sv.startstamp = app.db.wallClockTimeStamp(sv.startDate);
+        sv.duration = app.db.getElapsedTime(date, sv.startDate);
+        nowiso = date.toISOString();
+        sv.pts.forEach(function (pt) {
+            pt.visited = nowiso; });
+        sv.visited = nowiso;
+        d3.select("#suppvisdiv")
+            .style("visibility", "hidden");
+        endf();
+    }
+
+
+    function addToHomeScreen () {
+        jt.out("sv0txtdiv", jt.tac2html(
+            ["div", {id:"sv0howtodiv"}, 
+             ["Not implemented yet",
+              ["div", {id:"sv0buttonsdiv"},
+               ["button", {type:"button", cla:"ghostbutton",
+                           onclick:jt.fs("app.intro.hidetxtdiv()")},
+                "Ok"]]]]));
+        jt.byId("sv0txtdiv").style.opacity = 1.0;
+    }
+
+
+    function addToBookmarks () {
+        jt.out("sv0txtdiv", jt.tac2html(
+            ["div", {id:"sv0howtodiv"}, 
+             ["Not implemented yet",
+              ["div", {id:"sv0buttonsdiv"},
+               ["button", {type:"button", cla:"ghostbutton",
+                           onclick:jt.fs("app.intro.hidetxtdiv()")},
+                "Ok"]]]]));
+        jt.byId("sv0txtdiv").style.opacity = 1.0;
+    }
+
+
+    function displayActionLink () {
+        if(mobile) {
+            jt.out("sv0linkdiv", jt.tac2html(
+                ["a", {href:"#addToHomeScreen",
+                       onclick:jt.fs("app.intro.addToHomeScreen()")},
+                 "Add This Site To Your Home Screen"])); }
+        else {
+            jt.out("sv0linkdiv", jt.tac2html(
+                ["a", {href:"#addToBookmarks",
+                       onclick:jt.fs("app.intro.addToBookmarks()")},
+                 "Bookmark This Site"])); }
+        setTimeout(showCloseButton, 5000);
+    }
+
+
+    function initLinkText () {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        //windows phones also claim android and IOS.
+        if((/windows\sphone/i.test(userAgent)) ||
+           (/android/i.test(userAgent)) ||
+           (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)) {
+            mobile = true; }
+        //Removed statement about being speedier or more thorough.  Keep
+        //this concise
+        jt.out("sv0textdiv", "This will take more than one visit.");
+        setTimeout(displayActionLink, 1200);
+    }
+
+
+    function initInteractiveText () {
+        computeEstimatedCompletionTime();
+        updateCompletionTimeDisplay();
+        setTimeout(initLinkText, 3200);
     }
 
 
@@ -156,6 +321,9 @@ app.intro = (function () {
         vu.g = d3.select("#sv0usvg").append("g");
         makeBracketsAndTriangles(vu);
         animateBrackets(vu);
+        drawHorizontalLine(vu);
+        drawUnlockedText(vu);
+        setTimeout(initInteractiveText, vu.tim.txt.del + vu.tim.txt.dur - 200);
         d3.select("#suppvisdiv")
             .style("left", vu.cx - 15 + "px")
             .style("top", vu.cy - 15 + "px")
@@ -171,24 +339,20 @@ app.intro = (function () {
     }
 
 
-    function initAnimationSequence () {
-        //expand the box to show the unlock
-        //display the average completion time with countdown
-        //display the text and link
-    }
-
-
     function display (suppvis, timeline, endfunc) {
         sv = suppvis || app.lev.suppVisByCode("sl");
         tl = timeline || app.linear.tldata();
         endf = endfunc || app.dlg.close;
         sv.startDate = new Date();
         initDisplayElements();
-        initAnimationSequence();
     }
 
 
     return {
-        display: function (sv, tl, endf) { display(sv, tl, endf); }
+        display: function (sv, tl, endf) { display(sv, tl, endf); },
+        addToHomeScreen: function () { addToHomeScreen(); },
+        addToBookmarks: function () { addToBookmarks(); },
+        close: function () { close(); },
+        hidetxtdiv: function () { jt.byId("sv0txtdiv").style.opacity = 0; }
     };
 }());
