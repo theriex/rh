279b717b0c5b303ba85bc5fd0eeb9f0f0ac05030
Eric Parker <theriex@gmail.com>
2018-07-18 16:53:02 -0400

    Added point and timeline edits to /periodic

--- usracehistory-195216/src/py/service.py
+++ usracehistory-195216/src/py/service.py
@@ -2,6 +2,8 @@ import webapp2
 from google.appengine.ext import db
 import logging
 import appuser
+import point
+import timeline
 import datetime
 
 class AppService(db.Model):
@@ -29,9 +31,7 @@ class PublicPointIds(webapp2.RequestHandler):
         self.response.out.write(svc.data)
 
 
-def recent_access_accounts(days=1):
-    dtnow = datetime.datetime.utcnow()
-    thresh = appuser.dt2ISO(dtnow - datetime.timedelta(days))
+def recent_access_accounts(thresh):
     vq = appuser.VizQuery(appuser.AppUser, "ORDER BY accessed DESC")
     accs = vq.fetch(1000, read_policy=db.EVENTUAL_CONSISTENCY, deadline=20)
     names = ""
@@ -45,19 +45,53 @@ def recent_access_accounts(days=1):
     return names
 
 
+def recent_point_edits(thresh):
+    vq = appuser.VizQuery(point.Point, "ORDER BY modified DESC")
+    pts = vq.fetch(1000, read_policy=db.EVENTUAL_CONSISTENCY, deadline=20)
+    ptsums = ""
+    for pt in pts:
+        if pt.modified < thresh:
+            break
+        if ptsums:
+            ptsums += "\n"
+        ptsum = ("[ptid " + str(pt.key().id()) + "] " + pt.date + " " +
+                 pt.text[0:512])
+        logging.info(ptsum)
+        ptsums += ptsum
+    return ptsums
+
+
+def recent_timeline_edits(thresh):
+    vq = appuser.VizQuery(timeline.Timeline, "ORDER BY modified DESC")
+    tls = vq.fetch(1000, read_policy=db.EVENTUAL_CONSISTENCY, deadline=20)
+    tlsums = ""
+    for tl in tls:
+        if tl.modified < thresh:
+            break
+        if tlsums:
+            tlsums += "\n"
+        tlsum = ("[tlid " + str(tl.key().id()) + "] " + tl.name)
+        logging.info(tlsum)
+        tlsums += tlsum
+    return tlsums
+
+
 class PeriodicProcessing(webapp2.RequestHandler):
     def get(self):
         daysback = 3
         dtnow = datetime.datetime.utcnow()
         thresh = appuser.dt2ISO(dtnow - datetime.timedelta(daysback))
         body = "usracehistory access since " + thresh + "\n"
-        body += recent_access_accounts(daysback)
+        body += recent_access_accounts(thresh) + "\n"
+        body += recent_timeline_edits(thresh) + "\n"
+        body += recent_point_edits(thresh) + "\n"
         subj = "usracehistory activity report"
         try:
             appuser.mailgun_send(None, "support@usracehistory.org", subj, body)
         except:
             logging.info(subj + "\n\n" + body)
             raise
+        body += "\n-----------------------------"
         body += "\nPeriodicProcessing completed."
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write(body)
