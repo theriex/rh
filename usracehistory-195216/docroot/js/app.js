01fab2418c14d8f877e7cacf3b84ad591c6ca25b
Eric Parker <theriex@gmail.com>
2018-10-28 13:06:23 -0400

    Updated cache bust timestamp

--- usracehistory-195216/docroot/js/app.js
+++ usracehistory-195216/docroot/js/app.js
@@ -86,7 +86,7 @@ var app = {},  //Global container for application level funcs and values
         jt.out("loadstatdiv", "Loading app modules...");
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, modules, app.baseurl, app.init2, "?cbp=181019");
+        jt.loadAppModules(app, modules, app.baseurl, app.init2, "?cbp=181028");
     };
 
 
