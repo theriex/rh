e5ccc6d188e925d280f72d8c61bcbce68972c7f8
Eric Parker <theriex@gmail.com>
2019-09-30 11:19:30 -0400

    Updated script cache bust tag

--- usracehistory-195216/docroot/js/app.js
+++ usracehistory-195216/docroot/js/app.js
@@ -93,7 +93,7 @@ var jt = {};   //Global access to general utility methods
         jt.out("loadstatdiv", "Loading app modules...");
         app.amdtimer = {};
         app.amdtimer.load = { start: new Date() };
-        jt.loadAppModules(app, modules, app.baseurl, app.init2, "?cbp=190822");
+        jt.loadAppModules(app, modules, app.baseurl, app.init2, "?cbp=190930");
     };
 
 
