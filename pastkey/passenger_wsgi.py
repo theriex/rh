1d7d986ebd02f75cdfeb74034d81dd1f16ad411c
Eric Parker <theriex@gmail.com>
2021-12-04 13:30:13 -0500

    Switch to new deployment user

--- pastkey/passenger_wsgi.py
+++ pastkey/passenger_wsgi.py
@@ -1,8 +1,8 @@
 import sys, os
-INTERP = "/home/theriex/pastkey.org/venv/bin/python"
+INTERP = "USERHOME/pastkey.org/venv/bin/python"
 if sys.executable != INTERP:
-    # According to Dreamhost docs, INTERP is present twice so that the new
-    # Python interpreter knows the actual executable path
+    # INTERP is present twice so that the new Python interpreter knows
+    # the actual executable path
     os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
 from main import app as application
