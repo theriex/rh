b4e7104a4109d65c6ceb0cf6e86c5b440a167a82
Eric Parker <theriex@gmail.com>
2024-02-07 13:44:20 -0500

    Ported site from Passenger to FastCGI

--- pastkey/py/start.py
+++ pastkey/py/start.py
@@ -8,7 +8,7 @@
 import logging
 import py.util as util
 
-CACHE_BUST_PARAM = "v=211204"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=230618"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
